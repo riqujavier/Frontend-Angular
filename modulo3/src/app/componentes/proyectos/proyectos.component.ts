158e2f6afa0f0363e84ee1ee0cdaac6896868af5
riqujavier <riquelmejavierariel@gmail.com>
2022-11-10 13:44:41 -0300

    agregado del boton de edit y borrar a proyectos

--- modulo3/src/app/componentes/proyectos/proyectos.component.ts
+++ modulo3/src/app/componentes/proyectos/proyectos.component.ts
@@ -10,6 +10,52 @@ export class ProyectosComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+  
+  ocultarEdit0  () {
+    const x = document.getElementById("proyect1") ;
+    if (x!.contentEditable == "true") {
+      x!.contentEditable = "false";
+    
+    } else {
+      x!.contentEditable = "true";
+    }
+  }
+  
+
+  ocultarEdit  () {
+  const x = document.getElementById("proyect2") ;
+  if (x!.contentEditable == "true") {
+    x!.contentEditable = "false";
+  
+  } else {
+    x!.contentEditable = "true";
+  
+  }
+}
+
+ eliminarElemento(){
+	let imagen = document.getElementById('proyect1');	
+	if (!imagen){
+		alert("El elemento selecionado no existe");
+	} else {
+		let padre = imagen.parentNode;
+		padre!.removeChild(imagen);
+	}
+}
+
+
+eliminarElemento2(){
+	let imagen = document.getElementById('proyect2');	
+	if (!imagen){
+		alert("El elemento selecionado no existe");
+	} else {
+		let padre = imagen.parentNode;
+		padre!.removeChild(imagen);
+	}
+}
+
+
 
 }
